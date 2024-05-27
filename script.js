d7cc4ecaa626ef0e0771cc6c6e8a2151c198fc1a
teamamisimedishvili <127042618+teamamisimedishvili@users.noreply.github.com>
2024-05-27 15:58:18 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+
+   var object = document.getElementsByClassName("box");
+   
+
+
+// boxes[0]. innerHTML = 1;
+
+for ( var i = 0; i <= object.length; i ++ ) {
+
+    object[i]. innerHTML = i + 1;
+}
+
+
+
+
+// Collect all elements with the specified class names
+// var allObjects = [];
+// allObjects.push(...document.getElementsByClassName("box1"));
+// allObjects.push(...document.getElementsByClassName("box2"));
+// allObjects.push(...document.getElementsByClassName("box3"));
+// allObjects.push(...document.getElementsByClassName("box4"));
+// allObjects.push(...document.getElementsByClassName("box5"));
+
+// // Iterate over the collected elements and update their innerHTML
+// for (var i = 0; i < allObjects.length; i++) {
+    
+//     allObjects[i].innerHTML = i + 1;
+// }
